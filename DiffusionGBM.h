bde051cb831160eac848e728f6785b70be6927a2
Andrey-Shcherbakov <55910874+Andrey-Shcherbakov@users.noreply.github.com>
2021-02-05 19:31:50 +0300

    diffusions

---
+++ DiffusionGBM.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+
+//en.cppreference.com
+
+namespace siriusFM{
+	class DiffusionGBM{
+		private:
+			double const m_mu;
+			double const m_sigma;
+		public:
+			double mu(doubles a_s, double t){
+				return m_mu * a_s;
+			}
+
+			double sigma(doubles a_s, double t){
+				if(a_s < 0) return 0.0; //return((a_s < 0) ? 0 : m_sigma * a_s);
+				return m_sigma * a_s;
+			}
+			DiffusionGBM(double a_mu, double a_sigma)
+			: m_mu(s_mu)
+			  m_sigma(a_sigma){
+				if(m_sigma <= 0) throw std::invalid_arguement('...');
+			}
+	}
+}
---
+++ DiffusionOU.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+//en.cppreference.com
+
+namespace siriusFM{
+	class DiffusionOU{
+		private:
+			double const m_cappa;
+			double const m_sigma;
+			double const m_theta;
+		public:
+			double mu(doubles a_s, double t){
+				return m_cappa * (m_theta - a_s);
+			}
+
+			double sigma(doubles a_s, double t){
+				if(a_s < 0) return 0.0; //return((a_s < 0) ? 0 : m_sigma * a_s);
+				return m_sigma * a_s;
+			}
+			DiffusionOU(double a_cappa, double a_sigma, double a_theta)
+			: m_cappa(a_cappa)
+			  m_sigma(a_sigma)
+			  m_theta(a_theta){
+				if(m_sigma <= 0) throw std::invalid_arguement('...');
+				if(m_cappa < 0)  throw std::invalid_arguement('...');
+			}
+	}
+}
